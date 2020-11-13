5224fcbb8644c89fe550d840d8008726401458ca
Ryan K. Clark <mail.w3geek@gmail.com>
2020-11-13 12:47:07 -0500

    Removed 'posts' route. Added 'quote' route to accept a unique post slug as a route param and respond with applicable json otherwise 404.
    Route Wildcards - https://laracasts.com/series/laravel-6-from-scratch/episodes/7

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,36 @@
 	|
 	*/
 
-	Route::get('/posts/{slug}', function($slug) {
-		return $slug;
+	Route::get('/quotes/{slug}', function($slug) {
+		$quotes = [
+			'my-first-post' => [
+				'quote' => 'It’s dangerous to go alone, take this!',
+				'author' => 'Old Man',
+				'game' => 'The Legend of Zelda'
+			],
+
+			'my-second-post' => [
+				'quote' => 'War. War never changes.',
+				'author' => 'Narrator',
+				'game' => 'Fallout'
+			],
+
+			'my-third-post' => [
+				'quote' => 'Thank you Mario! But our Princess is in another castle!',
+				'author' => 'Toad',
+				'game' => 'Super Mario Bros.'
+			],
+
+			'secret-post' => [
+				'quote' => 'I used to be an adventurer like you, until I took an arrow to the knee.',
+				'author' => 'Town Guard',
+				'game' => 'Elder Scrolls V: Skyrim'
+			]
+		];
+
+		if (!array_key_exists($slug, $quotes)) {
+			abort(404, 'Sorry, that quote was not found.');
+		}
+
+		return $quotes[$slug];
 	});
